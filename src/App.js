c7629bb6749682cd39d46d07c78e65d789a5cbbe
ADITYA NARAYANA CHOUDHURY <69430444+adityasubham03@users.noreply.github.com>
2023-08-05 12:46:36 +0530

    Login Integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import './App.css';
+import "./App.css";
 import bgi from "./assets/bg.svg";
 
-import { BrowserRouter, Route,Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Login from "./page/loginpage.jsx";
-import Kiitp from './kiitp/kiitp.jsx';
+import Kiitp from "./kiitp/kiitp.jsx";
+import { UserContextProvider } from "./UserContext";
 
+const baseURL =
+	window.location.hostname === "frontend.unknownclub.me"
+		? "https://backend.unknownclub.me"
+		: "http://localhost:5000";
+// console.log(baseURL);
+
+axios.defaults.baseURL = baseURL;
+
+axios.defaults.withCredentials = true;
 
 function App() {
-  return (
-    <div className="App bg-cover" style={{backgroundImage: `url(${bgi})`, backgroundRepeat: 'no-repeat'}}>
-      <BrowserRouter>
-       <Routes>
-       <Route path='/k3/kiit' element={<Kiitp title="Kiit"/>}/>
-       <Route path="/" element={<Login title="Login"/>}/>    
-       
-       </Routes>
-       
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div
+			className="App bg-cover"
+			style={{ backgroundImage: `url(${bgi})`, backgroundRepeat: "no-repeat" }}>
+			<BrowserRouter>
+				<UserContextProvider>
+					<Routes>
+						<Route path="/k3/kiit" element={<Kiitp title="Kiit" />} />
+						<Route path="/" element={<Login title="Login" />} />
+					</Routes>
+				</UserContextProvider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,76 +1,110 @@
-import React from "react";
+import { React, useState, useContext } from "react";
+import { UserContext } from "../UserContext";
+import axios, { AxiosError } from "axios";
 
+const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [redirect, setRedirect] = useState(false);
+  const { setUser, user } = useContext(UserContext);
+  
+  console.log(user);
 
-const login = () => {
+	async function login(ev) {
+		ev.preventDefault();
+		try {
+      const res = await axios.post("/api/user/login", { username, password });
+      console.log(res);
+			if (res.status === 200) {
+        setUser(res.data.data);
+        alert("Login Successful.");
+        console.log("user"+user);
+				setRedirect(true);
+			}
+		} catch (err) {
+      if (err instanceof AxiosError) {
+        if (err.code === 'ERR_NETWORK') {
+          alert(err.name + "!! " + err.code + err.message);
+        } else {
+          alert(err.response.status + "!! " + err.response.data.message);
+        }
+				
+			}
+		}
+	}
+
+	// if (redirect) {
+	// 	return <Navigate to={"/"} />;
+	// }
+
   return (
-    <div className=" " >
-      <div className="flex justify-center h-screen items-center bg-cover shadow-lg "  >
-        <div className="w-full max-w-md items-center justify-center">
-          
-          <div className="#b1ff8a shadow-lg rounded-md p-8">
-            <h2 className="mt-6 text-center text-4xl font-bold tracking-tight text-gray-900">
-              Log In
-            </h2>
+  
+    <div className=" ">
+			<div className="flex justify-center h-screen items-center bg-cover shadow-lg ">
+				<div className="w-full max-w-md items-center justify-center">
+					<div className="#b1ff8a shadow-lg rounded-md p-8">
+						<h2 className="mt-6 text-center text-4xl font-bold tracking-tight text-gray-900">
+							Log In
+						</h2>
 
-            <form className="space-y-6 mt-6" action="#" method="POST">
-              <div className="text-left">
-                <label
-                  for="password"
-                  className="block text-base font-medium text-black"
-                >
-                  Employee id
-                </label>
-                <div className="mt-1">
-                  <input
-                    name="email"
-                    type="email-address"
-                    autocomplete="email-address"
+						<form className="space-y-6 mt-6" onSubmit={login}>
+							<div className="text-left">
+								<label
+									for="password"
+									className="block text-base font-medium text-black">
+									Employee Id/Username
+								</label>
+								<div className="mt-1">
+									<input
+										name="username"
+										type="text"
+										autoComplete="username"
                     required
-                    className="px-2 py-3 mt-1 block w-full text-black rounded-md border-4 bg-gray-300 border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-base"
-                  />
-                </div>
-              </div>
+                    value={username}
+                    onChange={(ev) => {
+											setUsername(ev.target.value);
+										}}
+										className="px-2 py-3 mt-1 block w-full text-black rounded-md border-4 bg-gray-300 border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-base"
+									/>
+								</div>
+							</div>
 
-              <div className="text-left">
-                <label
-                  for="password"
-                  className="block text-base font-medium text-black"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autocomplete="password"
+							<div className="text-left">
+								<label
+									for="password"
+									className="block text-base font-medium text-black">
+									Password
+								</label>
+								<div className="mt-1">
+									<input
+										id="password"
+										name="password"
+										type="password"
+										autoComplete="password"
                     required
-                    className="px-2 py-3 mt-1 block w-full text-black rounded-md border-4 bg-gray-300 border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-base"
-                  />
-                </div>
-                <span className="text-sm text-gray-400">
-                Forgot password?
-              </span>
-              </div>
-              
+                    value={password}
+                    onChange={(ev) => {
+											setPassword(ev.target.value);
+										}}
+										className="px-2 py-3 mt-1 block w-full text-black rounded-md border-4 bg-gray-300 border-gray-300 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-base"
+									/>
+								</div>
+								<span className="text-sm text-gray-400">Forgot password?</span>
+							</div>
 
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 mt-8 text-lg font-medium text-white shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-                >
-                  Log In
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        
-      </div>
-      
-      
-    </div>
-  );
+							<div>
+								<button
+									type="submit"
+									className="flex w-full justify-center rounded-md border border-transparent bg-green-400 py-2 px-4 mt-8 text-lg font-medium text-white shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
+									Log In
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default login;
+export default Login;
